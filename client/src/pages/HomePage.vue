85dbd5ef053c50dc0fa13573b719b5e097d32407
[SkySnkrs] <[skysnkrs@gmail.com]>
2024-12-18 11:49:17 -0700

    home page changes

--- client/src/pages/HomePage.vue
+++ client/src/pages/HomePage.vue
@@ -4,8 +4,11 @@
 
 <template>
   <section class="container-fluid">
-    <div class="row mt-5 text-center">
+    <div class="row mt-4 mb-4 p-3 text-center">
       <h1 class="text-light">Forging New Opportunities <span class="gradient-text">Together</span></h1>
+    </div>
+    <div class="row">
+      <img id="homeImage" src="../assets/img/backgroundImage.jpg" alt="">
     </div>
   </section>
 </template>
@@ -18,5 +21,17 @@
   -webkit-text-fill-color: transparent;
   font-weight: bold;
   text-transform: uppercase;
+}
+
+#homeImage {
+  object-fit: cover;
+  object-position: bottom;
+  height: 71dvh;
+  width: 100%;
+  justify-content: center;
+  padding: 0px;
+  filter: brightness(50%);
+  user-select: none;
+  -webkit-user-drag: none;
 }
 </style>
