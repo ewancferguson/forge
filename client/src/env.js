e0e1003423c977f311128e8e30bcc399658dccb0
Mike Nurisio <michaelnurisio@gmail.com>
2024-12-18 10:44:13 -0700

    removed '/' from client env to fix Auth issue

--- client/src/env.js
+++ client/src/env.js
@@ -3,4 +3,4 @@ export const baseURL = dev ? 'http://localhost:3000' : ''
 export const useSockets = false
 export const domain = 'dev-1t6ykrr6svu8teqk.us.auth0.com'
 export const clientId = 'JESKt933uiprVk6qFAaWFOxnqYXHFmnP'
-export const audience = 'https://auth.speedwagonfoundation.com/'
+export const audience = 'https://auth.speedwagonfoundation.com'
