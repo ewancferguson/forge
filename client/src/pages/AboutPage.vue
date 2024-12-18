4fc8ba7a7fdf37b705359237bafda04c4993fe44
Anthony Lee <anthonylee5519@gmail.com>
2024-12-18 10:59:08 -0700

    about page done except image & contributors

--- client/src/pages/AboutPage.vue
+++ client/src/pages/AboutPage.vue
@@ -3,9 +3,47 @@
 </script>
 
 <template>
-  <div class="about">
-    <h1 class="text-success-emphasis-light">How does the Forge Work?</h1>
+  <div class="container">
+    <div class="about">
+      <h1 class="text-lime my-5">How does the Forge Work?</h1>
+    </div>
+    <div class="ms-5">
+      <div class="d-flex ps-5 ms-5">
+        <p class="col-7 text-light fw-thin mb-5 fs-4">The Forge is a platform designed to bridge the gap between
+          consumers
+          and
+          local
+          businesses.
+          Consumers can post
+          their specific business needs—whether it's a repair job, a service, or a custom request—and businesses can
+          respond
+          in the comments with tailored offers, pricing, or availability. On the flip side, businesses can showcase
+          their
+          services and special offers, making it easier for consumers to discover and connect with the right
+          providers.
+        </p>
+      </div>
+    </div>
+    <span><img src="" alt=""></span>
+  </div>
+  <div class="container-fluid">
+
+    <div class="bg-grey row">
+      <h1 class="text-green m-5 col-3">Contributors</h1>
+    </div>
   </div>
 </template>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.text-lime {
+  color: #E3FFCC;
+}
+
+.text-green {
+  color: #142F32;
+}
+
+.bg-grey {
+  background-color: #D9D9D9;
+}
+</style>
