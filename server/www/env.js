d2b40a65c6c04c63fa38fbed24405560737f86bb
Mike Nurisio <michaelnurisio@gmail.com>
2024-12-18 10:53:59 -0700

    fixed server env.js

--- server/www/env.js
+++ server/www/env.js
@@ -1,3 +1,3 @@
 export const domain = 'dev-1t6ykrr6svu8teqk.us.auth0.com'
 export const clientId = 'JESKt933uiprVk6qFAaWFOxnqYXHFmnP'
-export const audience = 'https://auth.speedwagonfoundation.com/'
+export const audience = 'https://auth.speedwagonfoundation.com'
