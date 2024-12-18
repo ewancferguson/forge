b54bef7963c72a58f4995788d2501247fcd324f8
[SkySnkrs] <[skysnkrs@gmail.com]>
2024-12-18 09:33:39 -0700

    home page changes

--- client/src/pages/HomePage.vue
+++ client/src/pages/HomePage.vue
@@ -5,10 +5,9 @@
 <template>
   <div class="home flex-grow-1 d-flex flex-column align-items-center justify-content-center">
     <div class="home-card p-5 card align-items-center shadow rounded elevation-3">
-      <img src="@/assets/img/cw-circle-logo.png" alt="CodeWorks Logo"
-        class="rounded-circle">
+      <img src="@/assets/img/cw-circle-logo.png" alt="CodeWorks Logo" class="rounded-circle">
       <h1 class="my-5 bg-dark text-white p-3 rounded text-center">
-        Vue 3 Starter
+        This Worked
       </h1>
     </div>
   </div>
