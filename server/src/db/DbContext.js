fdb750dea8b4062193cb41c1d9efd379d0db1f9d
Mike Nurisio <michaelnurisio@gmail.com>
2024-12-18 09:50:58 -0700

    listing schema (need to fill out pictures and location), registered in dbcontext

--- server/src/db/DbContext.js
+++ server/src/db/DbContext.js
@@ -1,10 +1,12 @@
 import mongoose from 'mongoose'
 import { AccountSchema } from '../models/Account'
 import { ValueSchema } from '../models/Value'
+import { ListingSchema } from '../models/Listing';
 
 class DbContext {
   Values = mongoose.model('Value', ValueSchema);
   Account = mongoose.model('Account', AccountSchema);
+  Listing = mongoose.model('Listing', ListingSchema;)
 }
 
 export const dbContext = new DbContext()
---
+++ server/src/models/Listing.js
@@ -0,0 +1,24 @@
+import { Schema } from "mongoose";
+
+
+export const ListingSchema = new Schema({
+    creatorId: {Type: Schema.ObjectId, required: true, ref: 'Account'},
+    type: {type: String, Enumerator: ['construction', 'manufacturing', 'maintenance', 'utilities', 'agriculture', 'skilled trades', 'other'], required: true},
+    body: {type: String, required: true, minLength: 15, maxLength: 1000},
+    // pictures: {type: String}
+    // location: {}
+    isResolved: {type: Boolean, required: true, default: false},
+    minBudget: {type: Number, min: 0, max: 100000, required: true},
+    maxBudget: {type: Number, min: 0, max: 100000, required: true}
+},{
+    timestamps: true,
+    toJSON: {virtuals: true}
+}
+)
+
+ListingSchema.virtual('creator', {
+    localField: 'creatorId',
+    ref: 'Account',
+    foreignField: '_id',
+    justOne: true
+})
