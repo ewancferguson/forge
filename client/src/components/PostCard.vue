aa807b9a9025e644a3bd59c1504126228441590e
Anthony Lee <anthonylee5519@gmail.com>
2024-12-18 14:44:23 -0700

    displaying image, name, and created at on card

--- client/src/components/PostCard.vue
+++ client/src/components/PostCard.vue
@@ -12,13 +12,22 @@ defineProps({
 <template>
 <<<<<<< HEAD
 
-  <div class="rounded rounded-4 card bg-grey text-dark">
+  <div class="rounded rounded-4 card bg-secondary text-primary my-3">
     <div class="d-flex">
-      <img class="profile-img m-2 ms-4" :src="listing.creator.picture" alt="">
-      <span class="fw-bold">
-        @{{ listing.creator.name }}
-      </span>
-      {{ listing.creator.createdAt }}
+      <img class="profile-img m-3 ms-5" :src="listing.creator.picture" alt="">
+      <div class="d-flex align-items-center">
+        <b>
+          @{{ listing.creator.name }}
+        </b>
+        {{ listing.createdAt.getHours() }}h
+      </div>
+    </div>
+    <div class="ps-5">
+      <p>{{ listing.body }}</p>
+    </div>
+    <img class="img-fluid px-5 pb-5 listing-pictures" v-if="listing.pictures" :src="listing.pictures" alt="">
+    <div>
+      <p>{{ }}</p>
     </div>
 =======
   <div class="col-12">
@@ -29,13 +38,13 @@ defineProps({
 
 
 <style lang="scss" scoped>
-.bg-grey {
-  background-color: #D9D9D9;
-}
-
 .profile-img {
   border-radius: 100em;
   max-height: 3em;
   max-width: 100%;
+}
+
+.listing-pictures {
+  object-fit: cover;
 }
 </style>
