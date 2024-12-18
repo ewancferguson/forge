73b773f1a74228960d98c88c86423f0d5ab6587d
Anthony Lee <anthonylee5519@gmail.com>
2024-12-18 11:59:24 -0700

    about page styled more, listing model done

---
+++ client/src/models/Listing.js
@@ -0,0 +1,14 @@
+export class Listing{
+  constructor(data){
+    this.id = data.id
+    this.creatorId = data.creatorId
+    this.type = data.type
+    this.location = data.location
+    this.body = data.body
+    this.pictures = data.pictures
+    this.isResolved = data.isResolved
+    this.minBudget = data.minBudget
+    this.maxBudget = data.maxBudget
+    this.creator = data.creator
+  }
+}
--- client/src/pages/AccountPage.vue
+++ client/src/pages/AccountPage.vue
@@ -13,11 +13,11 @@ const account = computed(() => AppState.account)
       <div class="d-flex justify-content-between align-items-center">
         <div class="ps-5 ms-5">
           <img class="profile-img" :src="account.picture" alt="" />
-          <h1 class="text-primary text-capitalize"> {{ account.name }}</h1>
+          <h1 class="text-primary text-capitalize pt-3"> {{ account.name }}</h1>
           <p>{{ account.email }}</p>
           <button class="btn btn-success fw-bold text-primary py-3 mb-5 rounded-4 outline">EDIT ACCOUNT</button>
         </div>
-        <div class="card bg-primary text-light p-3 rounded-4 me-5 fw-bold">
+        <div class="card bg-green text-light p-3 pe-5 rounded-4 me-5 fw-bold">
           <a class="pb-5 text-light" :href="account.facebook">FACEBOOK</a>
           <a class="pb-5 text-light" :href="account.linkedIn">LINKEDIN</a>
           <a class="text-light" :href="account.website">COMPANY SITE</a>
@@ -26,6 +26,16 @@ const account = computed(() => AppState.account)
     </div>
     <div v-else>
       <h1 class="text-light mt-5">Loading... <i class="mdi mdi-loading mdi-spin"></i></h1>
+    </div>
+  </div>
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-md-8">
+        <h3 class="text-success p-5 mt-5">Recent Posts</h3>
+      </div>
+      <div class="col-md-3">
+        <h3 class="text-success p-5 mt-5">Reviews</h3>
+      </div>
     </div>
   </div>
 </template>
@@ -41,5 +51,9 @@ img {
 
 .bg-grey {
   background-color: #D9D9D9;
+}
+
+.bg-green {
+  background-color: #194348;
 }
 </style>
