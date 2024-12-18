e8d2372adb00fb5fc01a5b2280b82294279761a4
Mike Nurisio <michaelnurisio@gmail.com>
2024-12-18 10:20:11 -0700

    added properties to account schema

--- server/src/models/Account.js
+++ server/src/models/Account.js
@@ -6,9 +6,22 @@ export const AccountSchema = new Schema(
     subs: [{ type: String }],
     email: { type: String, lowercase: true, unique: true },
     name: { type: String, required: true },
-    picture: { type: String }
+    picture: { type: String },
     // NOTE If you wish to add additional properties do so here
+    isBusiness: {type: Boolean, required: true, default: false},
+    coverImg: {type: String, maxLength: 2000},
+    linkedIn: {type: String, maxLength: 2000},
+    facebook: {type: String, maxLength: 2000},
+    website: {type: String, maxLength: 2000},
+    reviewId: {type: Schema.ObjectId, required: true, ref: 'Rating'}
   },
   { timestamps: true, toJSON: { virtuals: true } }
 )
+
+AccountSchema.virtual('rating', {
+  localField: 'reviewId',
+  ref: 'Review',
+  foreignField: '_id',
+  justOne: true
+})
 
