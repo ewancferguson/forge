edf9eafed430808c5e5d9a01f70f1c0ae70c16f0
Anthony Lee <anthonylee5519@gmail.com>
2024-12-18 11:24:52 -0700

    removed required property on reviewId

--- server/src/models/Account.js
+++ server/src/models/Account.js
@@ -8,12 +8,12 @@ export const AccountSchema = new Schema(
     name: { type: String, required: true },
     picture: { type: String },
     // NOTE If you wish to add additional properties do so here
-    isBusiness: {type: Boolean, required: true, default: false},
-    coverImg: {type: String, maxLength: 2000},
-    linkedIn: {type: String, maxLength: 2000},
-    facebook: {type: String, maxLength: 2000},
-    website: {type: String, maxLength: 2000},
-    reviewId: {type: Schema.ObjectId, required: true, ref: 'Rating'}
+    isBusiness: { type: Boolean, required: true, default: false },
+    coverImg: { type: String, maxLength: 2000 },
+    linkedIn: { type: String, maxLength: 2000 },
+    facebook: { type: String, maxLength: 2000 },
+    website: { type: String, maxLength: 2000 },
+    reviewId: { type: Schema.ObjectId, ref: 'Rating' }
   },
   { timestamps: true, toJSON: { virtuals: true } }
 )
