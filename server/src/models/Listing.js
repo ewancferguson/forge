61bd1f02852a0470374276939b4ca2eaa1a06a31
Mike Nurisio <michaelnurisio@gmail.com>
2024-12-18 11:12:35 -0700

    decapitalized 'type' under creatorId property in server listing.js

--- server/src/models/Listing.js
+++ server/src/models/Listing.js
@@ -3,10 +3,10 @@ import { pointSchema } from "./Location";
 
 
 export const ListingSchema = new Schema({
-    creatorId: {Type: Schema.ObjectId, required: true, ref: 'Account'},
+    creatorId: {type: Schema.ObjectId, required: true, ref: 'Account'},
     type: {type: String, Enumerator: ['construction', 'manufacturing', 'maintenance', 'utilities', 'agriculture', 'skilled trades', 'other'], required: true},
     body: {type: String, required: true, minLength: 15, maxLength: 1000},
-    pictures: {type: [String], maxLength: 2000},
+    pictures: [{type: String, maxLength: 2000}],
     location: {type: pointSchema},
     isResolved: {type: Boolean, required: true, default: false},
     minBudget: {type: Number, min: 0, max: 100000, required: true},
