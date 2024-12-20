e70aab166404145321ea35ac98b5d2ad878db537
Mike Nurisio <michaelnurisio@gmail.com>
2024-12-20 11:36:19 -0700

    created services controller/service

---
+++ server/src/controllers/ServicesController.js
@@ -0,0 +1,9 @@
+import BaseController from "../utils/BaseController";
+
+
+export class ServicesController extends BaseController{
+    constructor(){
+        super('api/services')
+        this.router
+    }
+}
---
+++ server/src/services/ServicesService.js
@@ -0,0 +1,7 @@
+
+class ServicesService{
+
+}
+
+
+export const servicesService = new ServicesService()
