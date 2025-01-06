e7df28adb49147e8463d2ab1719a08bee53f252a
ewaaaaannnn <ewanferg01@gmail.com>
2025-01-06 14:09:44 -0700

    can now delete listings from details page

--- client/src/pages/ListingDetailPage.vue
+++ client/src/pages/ListingDetailPage.vue
@@ -2,9 +2,11 @@
 import { AppState } from '@/AppState';
 import CommentCard from '@/components/CommentCard.vue';
 import CommentForm from '@/components/CommentForm.vue';
+import { router } from '@/router';
 import { commentService } from '@/services/CommentService';
 import { likesService } from '@/services/LikesService';
 import { listingsService } from '@/services/ListingsService';
+import { postsService } from '@/services/PostsService';
 import { logger } from '@/utils/Logger';
 import Pop from '@/utils/Pop';
 import { computed, onMounted, reactive, watch } from 'vue';
@@ -45,7 +47,17 @@ const pictures = computed(() => {
     : [];
 });
 
-
+async function deleteListing(listingId) {
+  try {
+    const yes = await Pop.confirm(`Are you sure you would like to delete this listing?`)
+    if (!yes) return
+    await postsService.deleteListing(listingId)
+    router.push({ name: 'Posts' })
+  }
+  catch (error) {
+    Pop.error(error);
+  }
+}
 
 
 const localListing = reactive({
@@ -125,7 +137,7 @@ async function getCommentsbyListingId() {
                 class="mdi mdi-chat"></i></button>
           </div>
           <div v-if="account?.id == listings?.creator.id" class="align-self-center me-3">
-            <button class="button-delete">
+            <button @click="deleteListing(listings.id)" class="button-delete">
               <svg viewBox="0 0 448 512" class="svgIcon">
                 <path
                   d="M135.2 17.7L128 32H32C14.3 32 0 46.3 0 64S14.3 96 32 96H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H320l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32L53.2 467c1.6 25.3 22.6 45 47.9 45H346.9c25.3 0 46.3-19.7 47.9-45L416 128z">
--- client/src/services/PostsService.js
+++ client/src/services/PostsService.js
@@ -4,6 +4,12 @@ import { Listing } from "@/models/Listing.js"
 import { AppState } from "@/AppState.js"
 
 class PostsService {
+  async deleteListing(listingId) {
+    const response = await api.delete(`api/listings/${listingId}`)
+    logger.log(response.data)
+    const listingIndex = AppState.listings.findIndex(listing => listing.id == listingId)
+    AppState.listings.splice(listingIndex, 1)
+  }
   async createListing(postData) {
     const response = await api.post('api/listings', postData)
     logger.log('CREATED POST', response.data)
