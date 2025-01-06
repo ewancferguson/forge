08852b812441c6e191f6729d2b938e3aa2318047
Anthony Lee <anthonylee5519@gmail.com>
2025-01-06 10:01:29 -0700

    background changed on all pages

--- client/src/App.vue
+++ client/src/App.vue
@@ -7,7 +7,7 @@ import Navbar from './components/Navbar.vue';
   <header>
     <Navbar />
   </header>
-  <main>
+  <main class="gradient-background">
     <router-view />
   </main>
 </template>
@@ -17,5 +17,9 @@ import Navbar from './components/Navbar.vue';
 
 :root {
   --main-height: calc(100vh - 32px - 64px);
+}
+
+.gradient-background {
+  background: linear-gradient(to right, #142f32, #4f5749, #5d793e);
 }
 </style>
