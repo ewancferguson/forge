e8b5326ad9c208f718d4e4993b6075efffc19e9d
ewaaaaannnn <ewanferg01@gmail.com>
2025-01-06 10:07:41 -0700

    logged comments

--- client/src/services/CommentService.js
+++ client/src/services/CommentService.js
@@ -1,10 +1,12 @@
 import { Comment } from "@/models/Comment.js"
 import { api } from "./AxiosService.js"
 import { AppState } from "@/AppState.js"
+import { logger } from "@/utils/Logger.js"
 
 class CommentService {
   async getCommentsbyListingId(listingId) {
     const response = await api.get(`api/listings/${listingId}/comments`)
+    logger.log(response.data, 'got comments')
     const comments = response.data.map(commentPOJO => new Comment(commentPOJO))
     AppState.comments = comments
   }
