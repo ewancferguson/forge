7f7a38876b96646b1f47a0c5e161f9f77085f2f3
SkySnkrs <skysnkrs@gmail.com>
2025-01-06 09:42:41 -0700

    added button styling for post

--- client/src/components/Navbar.vue
+++ client/src/components/Navbar.vue
@@ -181,9 +181,15 @@ const account = computed(() => AppState.account)
 
       <!-- Footer Section for Login -->
       <div class="footer mt-auto d-flex justify-content-center text-center py-3">
-        <button @click="toggleSideMenu" data-bs-toggle="modal" data-bs-target="#createPostModal"
-          class="side-menu-item btn btn-primary text-light">
-          Create Post <i class="mdi mdi-plus-box"></i>
+        <button @click="toggleSideMenu" data-bs-toggle="modal" data-bs-target="#createPostModal" type="button"
+          class="button">
+          <span class="button__text">Add Post</span>
+          <span class="button__icon"><svg xmlns="http://www.w3.org/2000/svg" width="24" viewBox="0 0 24 24"
+              stroke-width="2" stroke-linejoin="round" stroke-linecap="round" stroke="currentColor" height="24"
+              fill="none" class="svg">
+              <line y2="19" y1="5" x2="12" x1="12"></line>
+              <line y2="12" y1="12" x2="19" x1="5"></line>
+            </svg></span>
         </button>
       </div>
     </div>
@@ -201,6 +207,66 @@ const account = computed(() => AppState.account)
 </template>
 
 <style scoped>
+.button {
+  position: relative;
+  width: 150px;
+  height: 40px;
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+  border: 1px solid #34974d;
+  background-color: #3aa856;
+}
+
+.button,
+.button__icon,
+.button__text {
+  transition: all 0.3s;
+}
+
+.button .button__text {
+  transform: translateX(30px);
+  color: #fff;
+  font-weight: 600;
+}
+
+.button .button__icon {
+  position: absolute;
+  transform: translateX(109px);
+  height: 40px;
+  width: 39px;
+  background-color: #34974d;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.button .svg {
+  width: 30px;
+  stroke: #fff;
+}
+
+.button:hover {
+  background: #34974d;
+}
+
+.button:hover .button__text {
+  color: transparent;
+}
+
+.button:hover .button__icon {
+  width: 148px;
+  transform: translateX(0);
+}
+
+.button:active .button__icon {
+  background-color: #2e8644;
+}
+
+.button:active {
+  border: 1px solid #2e8644;
+}
+
 .gradient-text {
   background: linear-gradient(to right, #142f32, #e3ffcc, #D9D9D9);
   background-clip: text;
