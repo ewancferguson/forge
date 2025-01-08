565833c8c59faffe9c88b21be03e55efd0588165
SkySnkrs <skysnkrs@gmail.com>
2025-01-08 16:07:09 -0700

    added contact section for messages

--- client/src/pages/MessagingPage.vue
+++ client/src/pages/MessagingPage.vue
@@ -3,11 +3,11 @@ import { ref } from 'vue';
 
 
 
-const Arrow = ref('mdi-arrow-left-bold-box');
-const hideContact = ref(false)
+const Arrow = ref('mdi-arrow-right-bold-box');
+const hideContact = ref(true)
 function hideContactPage() {
   hideContact.value = !hideContact.value
-  Arrow.value = Arrow.value === 'mdi-arrow-left-bold-box' ? 'mdi-arrow-right-bold-box' : 'mdi-arrow-left-bold-box';
+  Arrow.value = Arrow.value === 'mdi-arrow-right-bold-box' ? 'mdi-arrow-left-bold-box ms-1' : 'mdi-arrow-right-bold-box';
 }
 </script>
 
@@ -16,18 +16,98 @@ function hideContactPage() {
     <div class="contact-container">
       <div class="contact-search">
         <input type="text" placeholder="Search contacts..." />
-        <h4 class="selectable m-0">
-          <i class="mdi mdi-account-search"></i>
-        </h4>
+        <i class="mdi fs-3 btn mdi-account-search"></i>
       </div>
       <div class="chat-bubbles">
+        <div class="contact-info">
+          <div class="contact-card d-flex">
+            <img class="img-fluid"
+              src="https://i.seadn.io/gae/jCQAQBNKmnS_AZ_2jTqBgBLIVYaRFxLX6COWo-HCHrYJ1cg04oBgDfHvOmpqsWbmUaSfBDHIdrwKtGnte3Ph_VwQPJYJ6VFtAf5B?auto=format&dpr=1&w=1000"
+              alt="Creator's Name" />
+            <div class="contact">
+              <div class="d-flex">
+                <p class="contact-name">Dino Man</p>
+                <i class="mdi mdi-message-badge ms-2"></i>
+              </div>
+              <div class="contact-message">
+                <p>
+                  How is it going? How was your day?
+                </p>
+              </div>
+            </div>
+          </div>
+          <div class="contact-card d-flex">
+            <img class="img-fluid"
+              src="https://i.seadn.io/gae/jCQAQBNKmnS_AZ_2jTqBgBLIVYaRFxLX6COWo-HCHrYJ1cg04oBgDfHvOmpqsWbmUaSfBDHIdrwKtGnte3Ph_VwQPJYJ6VFtAf5B?auto=format&dpr=1&w=1000"
+              alt="Creator's Name" />
+            <div class="contact">
+              <div class="d-flex">
+                <p class="contact-name">Dino Man</p>
+                <i class="mdi mdi-message-badge ms-2"></i>
+              </div>
+              <div class="contact-message">
+                <p>
+                  How is it going? How was your day?
+                </p>
+              </div>
+            </div>
+          </div>
+          <div class="contact-card d-flex">
+            <img class="img-fluid"
+              src="https://i.seadn.io/gae/jCQAQBNKmnS_AZ_2jTqBgBLIVYaRFxLX6COWo-HCHrYJ1cg04oBgDfHvOmpqsWbmUaSfBDHIdrwKtGnte3Ph_VwQPJYJ6VFtAf5B?auto=format&dpr=1&w=1000"
+              alt="Creator's Name" />
+            <div class="contact">
+              <div class="d-flex">
+                <p class="contact-name">Dino Man</p>
+                <i class="mdi mdi-message-badge ms-2"></i>
+              </div>
+              <div class="contact-message">
+                <p>
+                  How is it going? How was your day?
+                </p>
+              </div>
+            </div>
+          </div>
+          <div class="contact-card d-flex">
+            <img class="img-fluid"
+              src="https://i.seadn.io/gae/jCQAQBNKmnS_AZ_2jTqBgBLIVYaRFxLX6COWo-HCHrYJ1cg04oBgDfHvOmpqsWbmUaSfBDHIdrwKtGnte3Ph_VwQPJYJ6VFtAf5B?auto=format&dpr=1&w=1000"
+              alt="Creator's Name" />
+            <div class="contact">
+              <div class="d-flex">
+                <p class="contact-name">Dino Man</p>
+                <i class="mdi mdi-message-badge ms-2"></i>
+              </div>
+              <div class="contact-message">
+                <p>
+                  How is it going? How was your day?
+                </p>
+              </div>
+            </div>
+          </div>
+          <div class="contact-card d-flex">
+            <img class="img-fluid"
+              src="https://i.seadn.io/gae/jCQAQBNKmnS_AZ_2jTqBgBLIVYaRFxLX6COWo-HCHrYJ1cg04oBgDfHvOmpqsWbmUaSfBDHIdrwKtGnte3Ph_VwQPJYJ6VFtAf5B?auto=format&dpr=1&w=1000"
+              alt="Creator's Name" />
+            <div class="contact">
+              <div class="d-flex">
+                <p class="contact-name">Dino Man</p>
+                <i class="mdi mdi-message-badge ms-2"></i>
+              </div>
+              <div class="contact-message">
+                <p>
+                  How is it going? How was your day?
+                </p>
+              </div>
+            </div>
+          </div>
 
+        </div>
       </div>
     </div>
     <div class="chat-container">
-      <button @click="hideContactPage" class="btn btn-success mt-5 sticky-bottom" style="position: absolute;">
-        <i class="fs-3 mdi icon" :class="[Arrow]"></i>
-      </button>
+      <div @click="hideContactPage" class="closeMenuButton mb-3 sticky-bottom" style="position: absolute;">
+        <i class="fs-1 mdi icon" :class="[Arrow]"></i>
+      </div>
     </div>
   </section>
 </template>
@@ -42,8 +122,12 @@ function hideContactPage() {
   grid-template-rows: 1fr;
   height: 84vh;
   border-radius: 2em;
-  background-color: white;
+  background-color: rgb(54, 79, 62);
   transition: all 0.4s ease;
+}
+
+.closeMenuButton:hover {
+  cursor: pointer;
 }
 
 #MessagingPage.hide-contacts {
@@ -54,11 +138,17 @@ function hideContactPage() {
   width: 100%;
 }
 
+.icon {
+  color: white;
+}
+
 .contact-container {
+  display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
   overflow-y: auto;
+  overflow-x: hidden;
   margin: 0 auto;
   color: #666;
   width: 100%;
@@ -66,41 +156,108 @@ function hideContactPage() {
 
 .contact-search {
   position: sticky;
+  top: 0;
+  margin-top: 0px;
   margin-bottom: auto;
-  padding: 1em;
+  padding: 1em 1em 0;
   display: flex;
   align-items: center;
   background: #f8f8f8;
-  border-radius: 2em 2em 0px 0px;
-  padding-top: 0px;
+  border-radius: 2em 2em 0 0;
   height: 20%;
+  width: 100%;
+  box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
 }
 
 .contact-search input {
+  flex: 1;
   background-color: #eee;
-  padding: 0.5em;
+  padding: 0.5em 1em;
   border: none;
   outline: none;
   border-radius: 10px 0 0 10px;
+  font-size: 1rem;
+  width: 100%;
 }
 
 .contact-search h4 {
   margin: 0;
   color: #666;
-  background: #eee;
+  background-color: #eee;
   border-radius: 0 10px 10px 0;
+  padding: 0.5em 1em;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 1rem;
+  height: 100%;
 }
 
 .chat-bubbles {
-  padding: 1em;
+  padding: 0.75em;
   align-items: center;
   background: #f8f8f8;
   overflow-y: hidden;
   border-radius: 0px 0px 2em 2em;
   margin: 0px;
+  width: 100%;
+  height: 80%;
+  overflow-x: hidden;
 }
 
+.contact-card {
+  display: flex;
+  align-items: center;
+  padding: 0.8em;
+  margin: 0.8em 0;
+  border-radius: 1em;
+  background-color: #fff;
+  border: 1px solid rgba(0, 0, 0, 0.1);
+  box-shadow: 2px 6px 10px rgba(0, 0, 0, 0.1);
+  transition: transform 0.2s, box-shadow 0.2s;
+  overflow: hidden;
+}
 
+.contact-card:hover {
+  transform: scale(1.03);
+  box-shadow: 2px 8px 15px rgba(0, 0, 0, 0.2);
+  cursor: pointer;
+}
+
+.contact-card img {
+  height: 55px;
+  aspect-ratio: 1/1;
+  border-radius: 50%;
+  object-fit: cover;
+}
+
+.contact-info {
+  flex: 1;
+}
+
+.contact-name {
+  font-weight: bold;
+  font-size: 1rem;
+  margin: 0 0 0.3em;
+  color: #333;
+}
+
+.contact-message {
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  max-width: 60%;
+  color: #666;
+}
+
+.contact-message p {
+  margin: 0;
+  font-size: 0.8rem;
+}
+
+.contact {
+  margin-left: 0.8em;
+}
 
 .chat-container {
   justify-content: baseline;
