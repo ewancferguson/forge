cb5735180b8264aed4c455a0e8f390b8124a0817
Anthony Lee <anthonylee5519@gmail.com>
2025-01-09 12:39:21 -0700

    added router link to login

--- client/src/components/Login.vue
+++ client/src/components/Login.vue
@@ -28,7 +28,7 @@ async function logout() {
         </div>
         <div class="dropdown-menu dropdown-menu-sm-end dropdown-menu-start p-0" aria-labelledby="authDropdown">
           <div class="list-group">
-            <router-link :to="{ name: 'Account' }">
+            <router-link v-if="account" :to="{ name: 'Profile', params: { profileId: account.id } }">
               <div class="list-group-item dropdown-item list-group-item-action">
                 Manage Account
               </div>
