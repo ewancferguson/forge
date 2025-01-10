d38ce42d2e6bf82f21040e3abf33c6b98a75c6fa
ewaaaaannnn <ewanferg01@gmail.com>
2025-01-10 10:19:48 -0700

    ReviewCard styled

--- client/src/components/ReviewCard.vue
+++ client/src/components/ReviewCard.vue
@@ -8,29 +8,29 @@ defineProps({
 
 
 <template>
-  <div class="reviewCard">
-    <div class="header">
-      <div>
-        <img class="profile-img" :src="reviewProp.creator.picture" alt="">
+  <div class="reviewCard card p-3 mb-3">
+    <div class="card-header d-flex align-items-center">
+      <div class="me-3">
+        <img class="profile-img rounded-circle" :src="reviewProp.creator.picture" alt="">
       </div>
       <div>
-        <div class="stars">
+        <div class="stars d-flex">
           <svg v-for="index in reviewProp.rating" :key="index" fill="currentColor" viewBox="0 0 20 20"
-            xmlns="http://www.w3.org/2000/svg">
+            xmlns="http://www.w3.org/2000/svg" class="me-1" style="width: 20px; height: 20px;">
             <path
               d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z">
             </path>
           </svg>
         </div>
-        <p class="name">{{ reviewProp.creator.name }}</p>
+        <p class="name mb-0 mt-2 fw-bold">{{ reviewProp.creator.name }}</p>
       </div>
     </div>
-
-
-    <p class="message">
-      {{ reviewProp.body }}
-    </p>
+    <div class="card-body">
+      <p class="message mb-0">{{ reviewProp.body }}</p>
+    </div>
   </div>
+
+
 </template>
 
 
@@ -123,6 +123,7 @@ img {
   max-width: 50%;
   margin-bottom: 0px;
   border-radius: 50%;
+  object-fit: cover;
 }
 
 
